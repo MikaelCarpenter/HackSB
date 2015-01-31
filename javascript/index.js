04dc70973a18dec2ec700b6a0fdce05844f08ee5
MikaelCarpenter <mikaelcarp.dev@gmail.com>
2015-01-31 07:42:12 -0800

    now pushes room size and free until

--- javascript/index.js
+++ javascript/index.js
@@ -9,13 +9,24 @@ $(function(){
 			Object.keys(result).map(function(value, index) {
 				for(var key in result[value]) {
 					var input = result[value][key][day][time];
+					var roomSize = result[value][key].size;
+					var timeStepsLeft = (19.5 - time);
+					for (var i=0; i <= timeStepsLeft; i+=0.5) {
+						var timeCheck = time + i;
+						if(result[value][key][day][timeCheck]) {
+							var freeUntil = timeCheck;
+							i = timeStepsLeft + 1;
+						}else if(i = timeStepsLeft){
+							var freeUntil = '20';
+						}
+					}
 					if(!input) {
-						results.push([value, key]);
+						results.push([value, key, roomSize, freeUntil]);
 					}
 				}
 			});
 
-			console.log(results);
+			console.log(results[0]);
 		});
 	});
 });
